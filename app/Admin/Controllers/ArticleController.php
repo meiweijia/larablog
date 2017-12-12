6888551c94b3ae2defd3f2aee840cfb748b592c5
梅渭甲 <meiweijia@vip.qq.com>
2017-12-12 22:43:05 +0800

    文章新增和编辑功能完善

--- app/Admin/Controllers/ArticleController.php
+++ app/Admin/Controllers/ArticleController.php
@@ -14,6 +14,7 @@ use App\Admin\Extensions\Form\MarkdownEditor;
 class ArticleController extends Controller
 {
     use ModelForm;
+
     /**
      * Index interface.
      *
@@ -77,6 +78,9 @@ class ArticleController extends Controller
             $grid->title('标题');
             $grid->author('作者');
             $grid->created_at('发布时间');
+            $grid->status('状态')->display(function ($status) {
+                return $status ? '发布' : '草稿';
+            });
         });
     }
 
@@ -90,13 +94,13 @@ class ArticleController extends Controller
         return Admin::form(Article::class, function (Form $form) {
 
             $form->display('id', 'ID');
-            $form->text('title','标题');
-            $form->text('author','作者')->default('admin');
-            $form->text('excerpt','摘要');
-            $form->text('keywords','关键字');//seo
-            $form->text('description','描述');//seo
+            $form->text('title', '标题');
+            $form->text('author', '作者')->default('admin');
+            $form->text('excerpt', '摘要');
+            $form->text('keywords', '关键字');//seo
+            $form->text('description', '描述');//seo
 
-            $form->markdown('content','内容');
+            $form->markdown('content', '内容')->rows(30);
 
 //            $form->select('categories','分类')->options(function ($id) {
 //                $user = Article::find($id);
@@ -107,7 +111,7 @@ class ArticleController extends Controller
 //            })->ajax('/admin/api/users');//todo 获取分类
 
 
-            $form->switch('status', '发布?');
+            $form->switch('status', '发布?')->default(1);
             $form->display('updated_at', '最后更新');
         });
     }
