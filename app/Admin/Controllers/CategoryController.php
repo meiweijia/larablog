fdfd1cd133f38a5e660fbdd937f26a65cce3317e
梅渭甲 <meiweijia@vip.qq.com>
2017-12-12 23:30:10 +0800

    分类功能实现

---
+++ app/Admin/Controllers/CategoryController.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace App\Admin\Controllers;
+
+use App\Http\Controllers\Controller;
+use Encore\Admin\Controllers\ModelForm;
+use Encore\Admin\Facades\Admin;
+use Encore\Admin\Form;
+use Encore\Admin\Layout\Column;
+use Encore\Admin\Layout\Content;
+use Encore\Admin\Layout\Row;
+use Encore\Admin\Tree;
+use Encore\Admin\Widgets\Box;
+use App\Models\Category;
+
+class CategoryController extends Controller
+{
+    use ModelForm;
+
+    /**
+     * Index interface.
+     *
+     * @return Content
+     */
+    public function index()
+    {
+        return Admin::content(function (Content $content) {
+            $content->header('分类');
+            $content->description('列表');
+
+            $content->row(function (Row $row) {
+                $row->column(6, $this->treeView()->render());
+
+                $row->column(6, function (Column $column) {
+                    $form = new \Encore\Admin\Widgets\Form();
+                    $form->action(admin_base_path('/categories'));
+
+                    $form->select('parent_id', trans('admin.parent_id'))->options(Category::selectOptions());
+                    $form->text('title', '分类名')->rules('required');
+                    $form->text('uri', trans('admin.uri'));
+
+                    $column->append((new Box('分类', $form))->style('success'));
+                });
+            });
+        });
+    }
+
+    /**
+     * Redirect to edit page.
+     *
+     * @param int $id
+     *
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function show($id)
+    {
+        return redirect()->route('categroy.edit', ['id' => $id]);
+    }
+
+    /**
+     * @return \Encore\Admin\Tree
+     */
+    protected function treeView()
+    {
+        return Category::tree(function (Tree $tree) {
+            $tree->disableCreate();
+
+            $tree->branch(function ($branch) {
+                $payload = "<strong>{$branch['title']}</strong>";
+
+                if (!isset($branch['children'])) {
+                    if (url()->isValidUrl($branch['uri'])) {
+                        $uri = $branch['uri'];
+                    } else {
+                        $uri = admin_base_path($branch['uri']);
+                    }
+
+                    $payload .= "&nbsp;&nbsp;&nbsp;<a href=\"$uri\" class=\"dd-nodrag\">$uri</a>";
+                }
+
+                return $payload;
+            });
+        });
+    }
+
+    /**
+     * Edit interface.
+     *
+     * @param string $id
+     *
+     * @return Content
+     */
+    public function edit($id)
+    {
+        return Admin::content(function (Content $content) use ($id) {
+            $content->header(trans('分类'));
+            $content->description('编辑');
+
+            $content->row($this->form()->edit($id));
+        });
+    }
+
+    /**
+     * Make a form builder.
+     *
+     * @return Form
+     */
+    public function form()
+    {
+        return Category::form(function (Form $form) {
+            $form->display('id', 'ID');
+
+            $form->select('parent_id', trans('admin.parent_id'))->options(Category::selectOptions());
+            $form->text('title', '分类名')->rules('required');
+            $form->text('uri', trans('admin.uri'));
+
+            $form->display('created_at', trans('admin.created_at'));
+            $form->display('updated_at', trans('admin.updated_at'));
+        });
+    }
+}
